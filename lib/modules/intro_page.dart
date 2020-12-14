7effd8f1557ebde7b63c696869f838f5ae36d9ca
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-12-14 10:46:01 -0300

    refactor: personaliza tela intro

--- lib/modules/intro_page.dart
+++ lib/modules/intro_page.dart
@@ -26,11 +26,53 @@ class _IntroPageState extends State<IntroPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff161d27),
-      body: Center(
-        child: SpinKitRipple(
-          size: 70.0,
-          color: Color(0xfffe972f),
-        ),
+      body: Stack(
+        children: [
+          SpinKitFadingCube(
+            size: 70.0,
+            color: Color(0xfffe972f),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 50.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Fit',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 44,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Dosis',
+                          letterSpacing: 5,
+                        ),
+                      ),
+                      Text(
+                        'Training',
+                        style: TextStyle(
+                          color: Color(0xfffe972f),
+                          fontSize: 44,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: 'Dosis',
+                          letterSpacing: 5,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'Carregando Aplicação ...',
+                    style: TextStyle(color: Colors.grey.shade500),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
