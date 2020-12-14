937e78964b71fc34e29709f6c885f88b23cdfa67
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-12-13 22:42:38 -0300

    fix: corrige visão statusbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:login_design/modules/login_page.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +7,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Define a StatusBar com tema Light - verificar se afeta outras screens
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return MaterialApp(
       title: 'Login Design',
       debugShowCheckedModeBanner: false,
