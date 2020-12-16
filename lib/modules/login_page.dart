c256cf9841e496f93a54a9ee042bfd65b124be07
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-12-16 00:42:07 -0300

    refactor? ajustes em android de tela pequena

--- lib/modules/login_page.dart
+++ lib/modules/login_page.dart
@@ -62,169 +62,171 @@ class _LoginPageState extends State<LoginPage> {
             ),
             child: Scaffold(
               backgroundColor: Colors.transparent,
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Wall',
+              body: Align(
+                alignment: Alignment.bottomCenter,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Fit',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 44,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Dosis',
+                              letterSpacing: 5,
+                            ),
+                          ),
+                          Text(
+                            'Training',
+                            style: TextStyle(
+                              color: Color(0xffE4A708),
+                              fontSize: 44,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Dosis',
+                              letterSpacing: 5,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Hora de começar o treino! Faça Login',
+                        style: TextStyle(color: Colors.grey.shade500),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        height: 60,
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: TextField(
+                          keyboardType: TextInputType.emailAddress,
                           style: TextStyle(
+                            fontSize: 16,
                             color: Colors.white,
-                            fontSize: 44,
-                            fontWeight: FontWeight.w600,
-                            fontFamily: 'Dosis',
-                            letterSpacing: 5,
                           ),
-                        ),
-                        Text(
-                          'Academia',
-                          style: TextStyle(
-                            color: Color(0xffE4A708),
-                            fontSize: 44,
-                            fontWeight: FontWeight.w600,
-                            fontFamily: 'Dosis',
-                            letterSpacing: 5,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Hora de começar o treino! Faça Login',
-                      style: TextStyle(color: Colors.grey.shade500),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      height: 60,
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: 'E-mail',
-                          hintStyle: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                          filled: true,
-                          fillColor: Color(0xff161d27),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.white,
+                          decoration: InputDecoration(
+                            hintText: 'E-mail',
+                            hintStyle: TextStyle(
+                              color: Colors.grey.shade500,
                             ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    Container(
-                      height: 60,
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        obscureText: true,
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: 'Senha',
-                          hintStyle: TextStyle(
-                            color: Colors.grey.shade500,
-                          ),
-                          filled: true,
-                          fillColor: Color(0xff161d27),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Esqueceu sua senha?',
-                      style: TextStyle(
-                        color: Color(0xfffe972f),
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      height: 60,
-                      width: double.infinity,
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: FlatButton(
-                        onPressed: () {
-                          mudaLoading();
-                          Timer(Duration(seconds: 3), () => goToHomePage());
-                        },
-                        color: Color(0xfffe972f),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: _isLoading
-                            ? SpinKitThreeBounce(
-                                size: 25.0,
+                            filled: true,
+                            fillColor: Color(0xff161d27),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
                                 color: Colors.white,
-                              )
-                            : Text(
-                                'ENTRAR',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
                               ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Primeira vez aqui?',
+                      SizedBox(height: 12),
+                      Container(
+                        height: 60,
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          obscureText: true,
                           style: TextStyle(
-                            color: Colors.grey.shade500,
                             fontSize: 16,
+                            color: Colors.white,
+                          ),
+                          decoration: InputDecoration(
+                            hintText: 'Senha',
+                            hintStyle: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
+                            filled: true,
+                            fillColor: Color(0xff161d27),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
                         ),
-                        SizedBox(
-                          width: 8,
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Esqueceu sua senha?',
+                        style: TextStyle(
+                          color: Color(0xfffe972f),
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          'Cadastra-se',
-                          style: TextStyle(
-                            color: Color(0xfffe972f),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        height: 60,
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: FlatButton(
+                          onPressed: () {
+                            mudaLoading();
+                            Timer(Duration(seconds: 3), () => goToHomePage());
+                          },
+                          color: Color(0xfffe972f),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
                           ),
+                          child: _isLoading
+                              ? SpinKitThreeBounce(
+                                  size: 25.0,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  'ENTRAR',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
                         ),
-                      ],
-                    ),
-                    // Espaçamento Final do dispositivo
-                    SizedBox(height: 50),
-                  ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Primeira vez aqui?',
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            'Cadastra-se',
+                            style: TextStyle(
+                              color: Color(0xfffe972f),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Espaçamento Final do dispositivo
+                      SizedBox(height: 30),
+                    ],
+                  ),
                 ),
               ),
             ),
